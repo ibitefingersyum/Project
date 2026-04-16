c577d4389e3eeed13f570202f53c755220f40f42
lex <4alexzheng@gmail.com>
2026-04-16 12:37:31 -0600

    added concepts cause i didn't completely do the diagram.
    woah crazy right

--- src/enemies.py
+++ src/enemies.py
@@ -4,3 +4,50 @@ import pygame
 import random
 import map
 import health
+
+# hitbox
+
+# sprite
+
+# equipt item
+
+# targeting
+
+# type of enemy
+
+# Soldat (Increased ) 3 Weight
+# Rook (Creates buildings) 2 Weight
+# Mortician (Minor Buffs, Heals other enemies) 2 Weight
+# Officer (Buffs other enemies) 2 Weight
+# Jaeger (Traps) 2 Weight
+# Lancer (Charges) 1 Weight | Heavy Lance forced | (No Survivalist interaction for simplicity)
+# Vanguard (Shield) 2 Weight | Shield forced |
+
+# Shock Troopers (Elites) Forced respective weapons (Set Perks)
+
+
+# Enemy perk
+#Survivalist (+1 Weight), unique class interactions. (+1 Buff Officer, +Ability use-rate for: Rook, Mortician, Jaeger)
+
+#Enemy Weapons
+    # 2 Weight:
+        # Judgement (High Damage, Mid Accuracy, -Low RoF) (rifle) | 1 bullet | 3s reload | 200 dmg | 21 rpm
+        # Prince (+Mid Damage, High Accuracy, Mid RoF) (rifle) | 7 bullets | 4s reload | 150 dmg | 60 rpm
+        # Jesse (Mid Damage, -Mid Accuracy, Mid+ RoF) (rifle) | 10 bullets | 5.5s reload | 130 dmg | 75 rpm
+        # Hellion (High Damage, -Low Accuracy, High RoF) (shotgun) |6 bullets | 7.75s reload | 22x9 dmg| 150 rpm
+
+    # 1 Weight:
+        # Grace Mid Damage, +Mid Accuracy, +Mid RoF (Revolver) | 6 Bullets | 4.75s reload | 120 dmg | 80 rpm
+        # Insurgent Low Damage, -Low Accuracy, ++High RoF (Handgun) | 20 Bullets | Can't Reload | 60 dmg | 230 rpm
+
+    # Special (1w): (Heavy Lance, Shield )
+        #
+
+
+# spawn radius
+
+# movement
+
+#interaction with map class
+
+#interaction with health class
