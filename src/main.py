68805f5551dc233bfdc62127c7985c6539b2e152
lex <4alexzheng@gmail.com>
2026-04-21 11:26:24 -0600

    a

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import pygame
+import random
+import map
+import health
+import vision
+import projectiles
+import player
+import enemies
+import hud
+import ui
+
+# This class acts as a game manager
+
